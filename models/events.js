a49d08f22a9d01c95114152e38eb9d0077e595ab
JojokCreator <joeyk2k@hotmail.com>
2022-06-21 15:33:22 +0100

    patch events

--- models/events.js
+++ models/events.js
@@ -14,15 +14,55 @@ export const getEvents = async() =>{
     );
     return data.rows;
   }
-  
-//   export async function updateTweet(id, updatedTweet) {
-//     const { name, tweet } = updatedTweet;
-//     const data = await query(
-//       `UPDATE tweets SET name = $1, tweet = $2 WHERE tweet_id = $3 RETURNING *;`,
-//       [name, tweet, Number(id)]
-//     );
-//     return data.rows;
-//   }
+
+  // UPDATE AN EVENT BY ID
+export const updateEvent = async(updatedEvent, id) => {
+    const { name_of_event, event_host, start_time, end_time, description,cost, address, lat, long, userAttending } = updatedEvent
+    const eventUpdate = await query(`SELECT * FROM events WHERE events_id = ${id}`)
+    if (name_of_event) {
+        await query (`UPDATE events SET name_of_event = $1 WHERE events_id = ${id};`,
+        [name_of_event] )
+    }
+    // if (event_host) {
+    //     await query (`UPDATE events SET event_host = $1 WHERE events_id = ${id};`,  //It doesn't like me changing this!!
+    //     [event_host] )
+    // }
+    if (start_time) {
+        await query (`UPDATE events SET start_time = $1 WHERE events_id = ${id};`,
+        [start_time] )
+    }
+    if ( end_time) {
+        await query (`UPDATE events SET end_time = $1 WHERE events_id = ${id};`,
+        [end_time] )
+    }
+    if ( description) {
+        await query (`UPDATE events SET description = $1 WHERE events_id = ${id};`,
+        [description] )
+    }
+    if ( cost) {
+        await query (`UPDATE events SET cost = $1 WHERE events_id = ${id};`,
+        [cost] )
+    }
+    if ( address) {
+        await query (`UPDATE events SET address = $1 WHERE events_id = ${id};`,
+        [address] )
+    }
+    if ( lat) {
+        await query (`UPDATE events SET lat = $1 WHERE events_id = ${id};`,
+        [lat] )
+    }
+    if ( long) {
+        await query (`UPDATE events SET long = $1 WHERE events_id = ${id};`,
+        [long] )
+    }
+    if ( userAttending) {
+        await query (`UPDATE events SET userAttending = $1 WHERE events_id = ${id};`,
+        [userAttending] )
+    }
+    if (eventUpdate) {
+        return eventUpdate.rows
+    } 
+}
   
   export async function deleteEvent(id) {
     const data = await query(
--- routes/events.js
+++ routes/events.js
@@ -1,35 +1,35 @@
 import express from "express";
-import { getEvents, createEvent, deleteEvent } from "../models/events.js";
+import { getEvents, createEvent, deleteEvent, updateEvent } from "../models/events.js";
 
 const eventsRouter = express.Router();
 // GET ALL EVENTS (GET)
 eventsRouter.get("/events", async(req, res)=>{
-    const result = await getEvents(); //models events 
+    const result = await getEvents(); 
     res.json({Success : true , Payload: result})
 })
 
 // //GET EVENT BY ID
-// eventsRouter.get("/events/:id", async(req, res)=>{
-//     const id = Number(req.params.id);
-//     const result = await getEventById(id); //models events 
-//     res.json({Success : true , Payload: result})
-// })
+eventsRouter.get("/events/:id", async(req, res)=>{
+    const id = Number(req.params.id);
+    const result = await getEventById(id); 
+    res.json({Success : true , Payload: result})
+})
 
 
 // //CREATE A NEW EVENT (POST)
-// eventsRouter.post("/events", async(req, res)=>{
-//     const newEvent = req.body 
-//     const result = await createEvent(newEvent);
-//     res.json({Success : true , Payload: result})
-// })
+eventsRouter.post("/events", async(req, res)=>{
+    const newEvent = req.body 
+    const result = await createEvent(newEvent);
+    res.json({Success : true , Payload: result})
+})
 
 // //UPDATE EVENT DETAILS (PATCH) 
-// eventsRouter.patch("/events/:id", async(req, res)=>{
-//     const id = Number(req.params.id);
-//     const updatedEvent = req.body 
-//     const result = await updateEvent(updatedEvent, id);
-//     res.json({Success : true , Payload: result})
-// })
+eventsRouter.patch("/events/:id", async(req, res)=>{
+    const id = Number(req.params.id);
+    const updatedEvent = req.body 
+    const result = await updateEvent(updatedEvent, id);
+    res.json({Success : true , Payload: result})
+})
 
 //DELETE AN EVENT (DELETE)
 
