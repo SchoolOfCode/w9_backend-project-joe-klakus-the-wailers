697b6be8600aee36493a4445e2e5b570ede7c78d
JojokCreator <joeyk2k@hotmail.com>
2022-06-21 14:37:33 +0100

    created events models for get, create and delete

--- models/events.js
+++ models/events.js
@@ -1,10 +1,33 @@
+import { query } from '../db/index.js';
 
-// export async function getEvents(){
-//     return [{userID : 1 , Name : "Joe"}, 
-//     {userID : 2 , Name : "Bob"}] 
-// }
-
+//Gets all the events from the database
 export const getEvents = async() =>{
-    return [{id : 1 , Name : "Joe"}, 
-    {id : 2 , Name : "Bob"}] 
-}
+    const data = await query(`SELECT * FROM events;`);
+    return data.rows;
+  }
+  //Creates a new event
+  export const createEvent = async(newEvent) => {
+    const { name_of_event, event_host, start_time, end_time, description,cost, address, lat, long, userAttending } = newEvent;
+    const data = await query(
+        `INSERT INTO events (name_of_event, event_host, start_time, end_time, description,cost, address, lat, long, userAttending) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *;`,
+      [name_of_event, event_host, start_time, end_time, description,cost, address, lat, long, userAttending]
+    );
+    return data.rows;
+  }
+  
+//   export async function updateTweet(id, updatedTweet) {
+//     const { name, tweet } = updatedTweet;
+//     const data = await query(
+//       `UPDATE tweets SET name = $1, tweet = $2 WHERE tweet_id = $3 RETURNING *;`,
+//       [name, tweet, Number(id)]
+//     );
+//     return data.rows;
+//   }
+  
+  export async function deleteEvent(id) {
+    const data = await query(
+      `DELETE FROM events WHERE events_id = $1 RETURNING *`,
+      [Number(id)]
+    );
+    return data.rows;
+  }
--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getEvents } from "../models/events.js";
+import { getEvents, createEvent, deleteEvent } from "../models/events.js";
 
 const eventsRouter = express.Router();
 // GET ALL EVENTS (GET)
@@ -8,28 +8,28 @@ eventsRouter.get("/events", async(req, res)=>{
     res.json({Success : true , Payload: result})
 })
 
-//GET EVENT BY ID
-eventsRouter.get("/events/:id", async(req, res)=>{
-    const id = Number(req.params.id);
-    const result = await getEventById(id); //models events 
-    res.json({Success : true , Payload: result})
-})
+// //GET EVENT BY ID
+// eventsRouter.get("/events/:id", async(req, res)=>{
+//     const id = Number(req.params.id);
+//     const result = await getEventById(id); //models events 
+//     res.json({Success : true , Payload: result})
+// })
 
 
-//CREATE A NEW EVENT (POST)
-eventsRouter.post("/events", async(req, res)=>{
-    const newEvent = req.body 
-    const result = await createEvent(newEvent);
-    res.json({Success : true , Payload: result})
-})
+// //CREATE A NEW EVENT (POST)
+// eventsRouter.post("/events", async(req, res)=>{
+//     const newEvent = req.body 
+//     const result = await createEvent(newEvent);
+//     res.json({Success : true , Payload: result})
+// })
 
-//UPDATE EVENT DETAILS (PATCH) 
-eventsRouter.patch("/events/:id", async(req, res)=>{
-    const id = Number(req.params.id);
-    const updatedEvent = req.body 
-    const result = await updateEvent(updatedEvent, id);
-    res.json({Success : true , Payload: result})
-})
+// //UPDATE EVENT DETAILS (PATCH) 
+// eventsRouter.patch("/events/:id", async(req, res)=>{
+//     const id = Number(req.params.id);
+//     const updatedEvent = req.body 
+//     const result = await updateEvent(updatedEvent, id);
+//     res.json({Success : true , Payload: result})
+// })
 
 //DELETE AN EVENT (DELETE)
 
