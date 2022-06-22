d81f713dee3dbcb009c2e64ef417c97c63874ecf
JojokCreator <joeyk2k@hotmail.com>
2022-06-22 11:01:00 +0100

    cors

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(express.json())
 //Sets the port
 const PORT = process.env.port || 5000;
 
+app.use(express.static('public'))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.0:5000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //Mock user for JWT
 const users = [
     {
