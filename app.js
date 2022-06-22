ad6725021e60f72fbff31dba8f8e29afbc3da383
Mustafa <Mustafa.mohamad239@gmail.com>
2022-06-22 11:49:04 +0100

    Cors error resolved

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.port || 5000;
 app.use(express.static('public'))
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.0:5000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
